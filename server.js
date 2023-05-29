2ebab4184dca5ea7ef76b313ad503ecebfc7cdbc
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-29 23:11:58 +0300

    define the server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+import router from './routes/index';
+
 const express = require('express');
-const routes = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/status', routes.AppController.getStatus);
-app.get('/stats', routes.AppController.getStats);
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+export default app;
