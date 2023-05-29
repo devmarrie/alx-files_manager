b94707adca7a6d225dbf1ebfdd11311a76d9e4ec
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-29 18:31:02 +0300

    users and counts numbers

--- utils/db.js
+++ utils/db.js
@@ -14,11 +14,29 @@ class DBClient {
   isAlive() {
     try {
       this.clientdb.connect();
+      this.db = this.clientdb.db();
       return true;
     } catch (error) {
       console.log(`Failed to connect ${error}`);
       return false;
     }
+  }
+
+  async nbUsers() {
+    try {
+      const collection = this.db.collection('users');
+      const count = await collection.countDocuments();
+      return count;
+    } catch (error) {
+      console.log(`No documents found ${error}`);
+      return -1;
+    }
+  }
+
+  async nbFiles() {
+    const collection = this.db.collection('files');
+    const count = await collection.countDocuments();
+    return count;
   }
 }
 
