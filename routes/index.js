d651f86ea6590b200bbcc795c8d4c6bc8a1a72bc
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-30 17:12:15 +0300

    post user route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = new Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
