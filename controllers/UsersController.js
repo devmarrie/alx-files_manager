e2b0c4f6ceae0b4f3c0db724813732d4ddddc635
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-31 00:07:35 +0300

    user controller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,28 @@
-const UsersController = {
+const dbUser = require('../utils/db');
 
+const UsersController = {
+  async postNew(req, res) {
+    const { email } = req.body;
+    const { password } = req.body;
+
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+    }
+    try {
+      const isExist = await dbUser.findUser(email);
+      if (isExist) {
+        res.status(400).json({ error: 'Already exist' });
+      } else {
+        const newUsr = await dbUser.newUser(email, password);
+        res.status(201).json({ id: newUsr._id, email });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  },
 };
 
 module.exports = UsersController;
