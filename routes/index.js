276001058f690d6ab15821cef2c42dfd20e6167e
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-31 00:07:55 +0300

    adding the user route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-module.exports = router;
+router.post('/users', UsersController.postNew);
 
+module.exports = router;
