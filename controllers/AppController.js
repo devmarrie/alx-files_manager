1e0f1e6d8b4474a421e3e90c837c34e44c39064e
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-30 21:42:41 +0300

    from a class to a const

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,19 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-class AppController {
-  static getStatus(req, res) {
+const AppController = {
+  getStatus(req, res) {
     res
       .status(200)
       .json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
-  }
+  },
 
-  static async getStat(req, res) {
+  async getStat(req, res) {
     const userstt = await dbClient.nbUsers();
     const filestt = await dbClient.nbFiles();
     res.status(200)
       .json({ users: userstt, files: filestt });
-  }
-}
+  },
+};
+
 module.exports = AppController;
