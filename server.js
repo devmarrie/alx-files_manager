006ba6baab52c1fdf10849e1f781c52033f598ce
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-30 22:10:56 +0300

    removing the app export

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
 app.use('/', router);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
-export default app;
