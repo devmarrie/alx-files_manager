d40d89143c4c216a131bfcd12f230271e6785fbb
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-31 10:24:35 +0300

    authenticate user and give them a token

---
+++ controllers/AuthController.js
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const crypto = require('crypto');
+const dbUser = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+const AuthController = {
+  async getConnect(res, req) {
+    const authHeader = req.headers.authorization;
+    const credentials = authHeader.replace('Basic', '');
+    // decode from base64
+    const decodeCred = Buffer.from(credentials, 'base64').toString('utf-8');
+    const [email, password] = decodeCred.split(':');
+    const user = dbUser.findUser(email);
+    const hashPass = crypto.createHash('sha1').update(password).digest('hex');
+    if (!user || user.password !== hashPass) {
+      res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      const token = uuidv4();
+      const key = `auth_${token}`;
+      await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+      res.status(200).json({ token });
+    }
+  },
+};
+
+module.exports = AuthController;
