f94a1c4ac1db985dca81928572b2d54716e78c10
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-31 17:41:20 +0300

    files route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
@@ -14,5 +15,8 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+// Files
+router.post('/files', FilesController.FilesController.postUpload);
 
 module.exports = router;
