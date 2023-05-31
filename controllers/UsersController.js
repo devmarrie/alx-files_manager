ef936e84949dabdd28de071b92d713d353a31683
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-31 14:07:00 +0300

    get me route

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 const dbUser = require('../utils/db');
+const redisClient = require('../utils/redis');
 
 const UsersController = {
   async postNew(req, res) {
@@ -21,6 +22,22 @@ const UsersController = {
       }
     } catch (error) {
       res.status(500).json({ message: error });
+    }
+  },
+
+  async getMe(req, res) {
+    const token = req.header('X-Token');
+    const key = `auth_${token}`;
+    const id = await redisClient.get(key);
+    if (id == null) {
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+    const stringify = id.toString();
+    const user = await dbUser.userByid(stringify);
+    if (!user) {
+      res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      res.status(200).json({ id: user._id, email: user.email });
     }
   },
 };
