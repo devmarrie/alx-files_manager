4b83994c4ec47d73703cafd818898ee75f6899fc
devmarrie <annemarrieakeyo42@gmail.com>
2023-05-31 10:24:58 +0300

    new token

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
 router.post('/users', UsersController.postNew);
+
+// Authenticate User
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
