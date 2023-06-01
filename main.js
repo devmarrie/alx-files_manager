b5bbd7b495b92e58b4d11fda27a3c83323fc71f7
devmarrie <annemarrieakeyo42@gmail.com>
2023-06-01 08:26:16 +0300

    commenting the aggregate

--- main.js
+++ main.js
@@ -29,17 +29,18 @@ const waitConnection = () => {
   console.log(await dbClient.findUser('john@doe.com'));
   console.log(await dbClient.userByid('6477052496fe7a4a7a8189b6'));
   // console.log(await dbClient.delFile('python.txt'));
-  // console.log(await dbClient.createFile('6477052496fe7a4a7a8189b6', 'java.txt', 'file', true, 0, '/tmp/files_manager'));
+  // console.log(await dbClient.createFile('6477052496fe7a4a7a8189b6', 'c++.txt', 'file', true, 0, '/tmp/files_manager'));
   // const base64String = Buffer.from('kenny@gmail.com:ken321').toString('base64');
   // console.log(base64String);
   // console.log(await dbClient.fileById('6477a1631de1715315089819'));
-  // console.log(await dbClient.fileBasedOnUid('6477052184d2084a4a087dbe'));
-  const pipeline = [
-    { $sort: { parentId: 1 } },
-    { $skip: 1 * 20 },
-    { $limit: 20 },
-  ];
-  console.log(await dbClient.paginate(pipeline));
+  console.log(await dbClient.fileBasedOnUid('6477052184d2084a4a087dbe'));
+  // console.log(await dbClient.updateFile('64778b784a9efe1954551d76,'));
+//   const pipeline = [
+//     { $sort: { parentId: 1 } },
+//     { $skip: 1 * 20 },
+//     { $limit: 20 },
+//   ];
+//   console.log(await dbClient.paginate(pipeline));
 })();
 // a2VubnlAZ21haWwuY29tOmtlbjMyMQ==
 // e10ea04f-04a3-4266-b7e8-198d95775177
